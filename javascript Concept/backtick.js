194354ccf1d04c60581048313571b66a9bb5b49b
unknown <priyanka.1711kumari@gmail.com>
2024-05-02 23:50:04 +0530

    javascript concept folder added

---
+++ javascript Concept/backtick.js
@@ -0,0 +1,12 @@
+ // use 1
+ let variable = "value";
+ let interpolatedString = `This is a ${variable} interpolated string.`;
+console.log( interpolatedString);
+
+//use 2-to print in given format
+ let abc=
+ `hi
+ my name is Priyanka
+       helloooooo
+ `;
+ console.log(abc);
