d657b9d3568326fd76c1035baf8dbe818ba79452
unknown <priyanka.1711kumari@gmail.com>
2024-05-13 14:18:55 +0530

    types of function in javascript

---
+++ javascript_basic/Function/function_type.js
@@ -0,0 +1,25 @@
+//without parameter and without return
+   function print(){
+    console.log('hello')
+   }
+   print();
+//without parameter and with return
+function print1(){
+    return 'hello priyanka'
+   }
+//  let abc=print1()
+ console.log(print1())
+
+//with parameter and without return
+function add(a,b){
+console.log("sum:" + (a+b));
+   }
+
+ add(3,9);
+
+//with parameter and with return
+function multi(c,d){
+    return (c*d);
+   }
+//  let abc=print1()
+ console.log(multi(2,7))
